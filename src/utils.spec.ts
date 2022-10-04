aa9a51b725072464f8c8739ec5344c47a9e4ed43
VonStroheim <stroheimdesign@gmail.com>
2022-10-04 19:09:08 +0200

    Update utils.spec.ts

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -28,6 +28,29 @@ describe("utils", () => {
         .filter(str => Boolean(str))
         .join("\n");
     };
+    
+    const buildICalMessageMultiple = (
+      title1: string,
+      title2: string,
+      startTime1: string,
+      startTime2: string
+    ) => {
+      return [
+        "BEGIN:VCALENDAR",
+        "VERSION:2.0",
+        "BEGIN:VEVENT",
+        `DTSTART:${formatDate(startTime1)}`,
+        `SUMMARY:${title1}`,
+        "END:VEVENT",
+        "BEGIN:VEVENT",
+        `DTSTART:${formatDate(startTime2)}`,
+        `SUMMARY:${title2}`,
+        "END:VEVENT",
+        "END:VCALENDAR"
+      ]
+        .filter(str => Boolean(str))
+        .join("\n");
+    };
 
     it("should throw error when both the required fields are missing", () => {
       // @ts-ignore
@@ -38,6 +61,19 @@ describe("utils", () => {
       const startTime = "2018-10-07T10:30:00+10:00";
       const result = buildUrl({ title, startTime });
       expect(result).toBe(buildICalMessage(title, startTime));
+    });
+    it("should return iCalendar string with multiple events when array of events is provided", () => {
+      const title1 = "Event 1";
+      const title2 = "Event 2";
+      const startTime1 = "2018-10-07T10:30:00+10:00";
+      const startTime2 = "2018-10-07T12:30:00+10:00";
+      const result = buildUrl(
+        [
+          { title1, startTime1 },
+          { title2, startTime2 }
+        ]
+      );
+      expect(result).toBe(buildICalMessageMultiple(title1, title2, startTime1, startTime2));
     });
     it("should include attendees in the iCalendar when the info is provided", () => {
       const title = "Something great";
